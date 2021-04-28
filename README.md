018d38383abe2a91b32e3510176e35d633959ac5
md.delwarhossain <dhossain@testreach.com>
2021-04-28 12:04:34 +0600

    add item module

--- README.md
+++ README.md
@@ -71,3 +71,6 @@ Nest is an MIT-licensed open source project. It can grow thanks to the sponsors 
 ## License
 
 Nest is [MIT licensed](LICENSE).
+
+need to see the permission to the script
+chmod +rx src/scripts/start-db.sh 
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { configService } from './config/config.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ItemModule } from './item/item.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+    ItemModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
